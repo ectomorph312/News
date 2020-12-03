a8861caf7141ff54b9c27dee0fd66227fa307dd2
ectomorph <ectomorph312@gmail.com>
2020-12-03 23:32:34 +0600

    initial commit

---
+++ app/src/main/java/com/example/task/Details.java
@@ -0,0 +1,67 @@
+package com.example.task;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+public class Details extends AppCompatActivity {
+
+    TextView tvTitle, tvSource, tvDesc, tvDate;
+    ImageView imageView;
+    WebView webView;
+    ProgressBar loader;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details);
+
+        tvTitle = findViewById(R.id.tvTitle);
+        tvSource = findViewById(R.id.tvSource);
+        tvDesc = findViewById(R.id.tvDecs);
+        tvDate = findViewById(R.id.tvDate);
+
+        imageView = findViewById(R.id.imageView);
+
+        webView = findViewById(R.id.webView);
+
+        loader = findViewById(R.id.webViewLoader);
+        loader.setVisibility(View.VISIBLE);
+
+        Intent intent = getIntent();
+        String title = intent.getStringExtra("title");
+        String source = intent.getStringExtra("source");
+        String date = intent.getStringExtra("date");
+        String desc = intent.getStringExtra("desc");
+        String imageUrl = intent.getStringExtra("imageUrl");
+        String url = intent.getStringExtra("url");
+
+        tvTitle.setText(title);
+        tvSource.setText(source);
+        tvDesc.setText(desc);
+        tvDate.setText(date);
+
+        Picasso.with(Details.this).load(imageUrl).into(imageView);
+
+        webView.getSettings().setDomStorageEnabled(true);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setLoadsImagesAutomatically(true);
+        webView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl(url);
+
+        if(webView.isShown()) {
+            loader.setVisibility(View.INVISIBLE);
+        }
+
+    }
+}
